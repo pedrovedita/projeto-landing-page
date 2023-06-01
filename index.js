bce394ab414fd2d355db0bafcc572c94a644bf9d
pedrovedita <pedrovbportella@gmail.com>
2023-06-01 00:37:14 -0300

    versão inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita(){
+    Leonardo.style = "display:none"
+    Bruna.style = "display:flex"
+    setaEsquerda.style = "display:flex"
+    setaDireita.style = "display:none"
+} 
+function RolarParaEsquerda(){
+    Leonardo.style = "display: flex"
+    Bruna.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+}
